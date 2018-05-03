c511610361e2e99d2aa162f8f02b4118beaf0fc8
bogdan.mihart <bogdan.mihart@smart2pay.com>
2018-05-03 19:03:24 +0300

    Added sample notification for asp.net core 1.1

---
+++ src/S2p.RestClient.Sdk.SampleNotification.NetCore/Controllers/NotificationController.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using S2p.RestClient.Sdk.Notifications;
+using S2p.RestClient.Sdk.SampleNotification.NetCore.Infrastructure;
+
+namespace S2p.RestClient.Sdk.SampleNotification.NetCore.Controllers
+{
+    public class NotificationController : Controller
+    {
+        private readonly INotificationProcessor _notificationProcessor;
+
+        public NotificationController(INotificationProcessor notificationProcessor)
+        {
+            _notificationProcessor = notificationProcessor;
+        }
+
+        [HttpPost]
+        [Route("api/notification/globalpay")]
+        public async Task<IActionResult> GlobalPay()
+        {
+            //do not declare any method parameters, you will neeed to read the request body as a string
+            var notificationBody = await Request.ReadBodyAsStringAsync();
+
+            //the notification processor will examine the json string and provide deserialization
+            var httpResult = await _notificationProcessor.ProcessNotificationBodyAsync(notificationBody);
+
+            return StatusCode((int)httpResult);
+        }
+    }
+}
---
+++ src/S2p.RestClient.Sdk.SampleNotification.NetCore/Models/NotificationCallback.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using S2p.RestClient.Sdk.Entities;
+using S2p.RestClient.Sdk.Notifications;
+
+namespace S2p.RestClient.Sdk.SampleNotification.NetCore.Models
+{
+    public class NotificationCallback : INotificationCallback
+    {
+        private readonly ILogger _logger;
+
+        public NotificationCallback(ILogger<NotificationCallback> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task<bool> CardPaymentNotificationCallbackAsync(ApiCardPaymentResponse cardPaymentNotification)
+        {
+            try
+            {
+                //simulate asynchronous work, here you will write your own business logic
+                await Task.Delay(50);
+
+                //signal operation success by returning true
+                return true;
+            }
+            catch (Exception ex)
+            {
+                // log the exception
+                _logger.LogError(ex.Message);
+
+                //signal operation failure by returning false
+                return false;
+            }
+        }
+
+        public async Task<bool> CardPayoutNotificationCallbackAsync(ApiCardPayoutResponse cardPayoutNotification)
+        {
+            try
+            {
+                //simulate asynchronous work, here you will write your own business logic
+                await Task.Delay(50);
+
+                //signal operation success by returning true
+                return true;
+            }
+            catch (Exception ex)
+            {
+                // log the exception
+                _logger.LogError(ex.Message);
+
+                //signal operation failure by returning false
+                return false;
+            }
+        }
+
+        public async Task<bool> DisputeNotificationCallbackAsync(ApiDisputeResponse disputeNotification)
+        {
+            try
+            {
+                //simulate asynchronous work, here you will write your own business logic
+                await Task.Delay(50);
+
+                //signal operation success by returning true
+                return true;
+            }
+            catch (Exception ex)
+            {
+                // log the exception
+                _logger.LogError(ex.Message);
+
+                //signal operation failure by returning false
+                return false;
+            }
+        }
+
+        public async Task<bool> InvalidFormatNotificationCallbackAsync(InvalidFormatNotification invalidFormatNotification)
+        {
+            try
+            {
+                //simulate asynchronous work, here you will write your own business logic
+                await Task.Delay(50);
+
+                //signal operation success by returning true
+                return true;
+            }
+            catch (Exception ex)
+            {
+                // log the exception
+                _logger.LogError(ex.Message);
+
+                //signal operation failure by returning false
+                return false;
+            }
+        }
+
+        public async Task<bool> PaymentNotificationCallbackAsync(ApiPaymentResponse paymentNotification)
+        {
+            try
+            {
+                //simulate asynchronous work, here you will write your own business logic
+                await Task.Delay(50);
+
+                //signal operation success by returning true
+                return true;
+            }
+            catch (Exception ex)
+            {
+                // log the exception
+                _logger.LogError(ex.Message);
+
+                //signal operation failure by returning false
+                return false;
+            }
+        }
+
+        public async Task<bool> PreapprovalNotificationCallbackAsync(ApiPreapprovalResponse preapprovalNotification)
+        {
+            try
+            {
+                //simulate asynchronous work, here you will write your own business logic
+                await Task.Delay(50);
+
+                //signal operation success by returning true
+                return true;
+            }
+            catch (Exception ex)
+            {
+                // log the exception
+                _logger.LogError(ex.Message);
+
+                //signal operation failure by returning false
+                return false;
+            }
+        }
+
+        public async Task<bool> RefundNotificationCallbackAsync(ApiRefundResponse refundNotification)
+        {
+            try
+            {
+                //simulate asynchronous work, here you will write your own business logic
+                await Task.Delay(50);
+
+                //signal operation success by returning true
+                return true;
+            }
+            catch (Exception ex)
+            {
+                // log the exception
+                _logger.LogError(ex.Message);
+
+                //signal operation failure by returning false
+                return false;
+            }
+        }
+
+        public async Task<bool> UnknownTypeNotificationCallbackAsync(UnknownTypeNotification unknownTypeNotification)
+        {
+            try
+            {
+                //simulate asynchronous work, here you will write your own business logic
+                await Task.Delay(50);
+
+                //signal operation success by returning true
+                return true;
+            }
+            catch (Exception ex)
+            {
+                // log the exception
+                _logger.LogError(ex.Message);
+
+                //signal operation failure by returning false
+                return false;
+            }
+        }
+    }
+}
